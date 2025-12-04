7deb9c4f8c1493ec6a56dc1e9b36ba9edb526cc6
Nimal N Rahuman <nimalnr2@gmail.com>
2025-12-04 10:38:22 +0530

    Add initial SQL script to insert student details into the database

---
+++ SQL1.sql
@@ -0,0 +1,2 @@
+INSERT INTO `student_details`(`Id`, `Name`, `Age`, `Department`, `Grade`) 
+VALUES ('1001','john','22','CSE','80'),('1002','jack','24','CE','92'),('1003','Sparrow','23','CSE','68'),('1004','don','22','ME','76'),('1005','appu','21','CSE','95');
